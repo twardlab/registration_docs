f50a9fee1b1b306b725f8f73239a534584095ead
Andrew Bennecke <jandrew624@hotmail.com>
2024-08-14 11:56:09 -0700

    Reorganized repo

---
+++ docs/.ipynb_checkpoints/conf-checkpoint.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Mouse Brain Atlas Registration'
+copyright = '2024, Andrew Bennecke'
+author = 'Andrew Bennecke and Daniel Tward'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'nbsphinx',
+    'sphinxcontrib.htmlhelp'
+]
+
+# Configuration for LaTeX output
+latex_documents = [
+    ('index', 'registration_docs.tex', project, author, 'manual'),
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+import sys
+sys.path.append('/home/abenneck/Desktop/image_reg/dragonfly_registration_summer_2023/registration_scripts')
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'nature'
+
+html_static_path = ['_static']
